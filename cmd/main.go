8df9b3a24267e70d093db03bec93f7488039993e
DylanGuedes <djmgguedes@gmail.com>
2020-06-28 14:07:22 -0300

    Add parser and mod structure

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,23 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/DylanGuedes/weakdb/core"
 )
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Welcome to shitdb.")
+	fmt.Println("Welcome to weakdb.")
 	for {
 		fmt.Print("# ")
 		text, _ := reader.ReadString('\n')
 		text = strings.Replace(text, "\n", "", -1)
+
+		_, err := core.Parse(text)
+
+		if err != nil {
+			panic(err)
+		}
 
 		fmt.Println("ok")
 	}
---
+++ core/parser.go
@@ -0,0 +1,7 @@
+package core
+
+// Parse parse the input string and return its AST representation.
+func Parse(source string) (string, error) {
+	// (TODO) to be implemented
+	return "", nil
+}
